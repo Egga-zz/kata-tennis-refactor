3e5c4b41ed20cb7a21e0a072534b769949f50d93
Gunnar Bastkowski <gunnar@bastkowski.name>
2015-12-10 21:00:49 +0100

    Duplicate code

--- src/main/java/de/egga/TennisGame1.java
+++ src/main/java/de/egga/TennisGame1.java
@@ -59,17 +59,17 @@ public class TennisGame1 implements TennisGame {
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
+            for (int i=1; i<3; i++) {
                 int tempScore;
-                if (i==1) {
+                if (i == 1) {
                     tempScore = a1;
+                    score += findToBeAdded(tempScore);
                 } else {
                     score += "-";
                     tempScore = b1;
+                    score += findToBeAdded(tempScore);
                 }
 
-                score += findToBeAdded(tempScore);
             }
         }
         return score;
