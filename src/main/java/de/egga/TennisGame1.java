8fde274901d071177cd4788bde01f29f9ac98975
Gunnar Bastkowski <gunnar@bastkowski.name>
2015-12-10 20:27:59 +0100

    Extract constants

--- src/main/java/de/egga/TennisGame1.java
+++ src/main/java/de/egga/TennisGame1.java
@@ -2,48 +2,46 @@ package de.egga;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+    public static final String LOVE_ALL = "Love-All";
+    public static final String FIFTEEN_ALL = "Fifteen-All";
+    public static final String THIRTY_ALL = "Thirty-All";
+    public static final String DEUCE = "Deuce";
+    private int a = 0;
+    private int b = 0;
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if ("player1".equals(playerName)) {
+            a += 1;
+        } else {
+            b += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (a == b)
         {
-            switch (m_score1)
+            switch (a)
             {
                 case 0:
-                    score = "Love-All";
+                    score = LOVE_ALL;
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score = FIFTEEN_ALL;
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score = THIRTY_ALL;
                     break;
                 default:
-                    score = "Deuce";
+                    score = DEUCE;
                     break;
 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (a >=4 || b >=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = a - b;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -53,8 +51,8 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = a;
+                else { score+="-"; tempScore = b;}
                 switch(tempScore)
                 {
                     case 0:
