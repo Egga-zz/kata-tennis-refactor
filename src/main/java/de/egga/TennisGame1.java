b33e5781f820eae5d4ad3bb510e76ea0c5a8bd6f
Philipp Eder <ederphpp@gmail.com>
2015-12-13 22:11:52 +0100

    got rid of for loop

--- src/main/java/de/egga/TennisGame1.java
+++ src/main/java/de/egga/TennisGame1.java
@@ -1,8 +1,5 @@
 package de.egga;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class TennisGame1 implements TennisGame {
 
     private int m_score1 = 0;
@@ -27,11 +24,8 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        if (m_score1 == m_score2) {
+            switch (m_score1) {
                 case 0:
                     score = "Love-All";
                     break;
@@ -46,24 +40,15 @@ public class TennisGame1 implements TennisGame {
                     break;
 
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                score += SCORE_NAMES[tempScore];
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            return SCORE_NAMES[m_score1] + "-" + SCORE_NAMES[m_score2];
 
-            }
         }
         return score;
     }
