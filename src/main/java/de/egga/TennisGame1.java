2c78d140b68a2993d3ccb1598f6cce696508db1d
Philipp Eder <ederphpp@gmail.com>
2015-12-13 22:13:19 +0100

    created edge case for "deuce"

--- src/main/java/de/egga/TennisGame1.java
+++ src/main/java/de/egga/TennisGame1.java
@@ -24,29 +24,17 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
+        if (m_score1 == 3 && m_score2 == 3 || m_score1 == 4 && m_score2 == 4)
+            return "Deuce";
+        if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
             if (minusResult == 1) score = "Advantage player1";
             else if (minusResult == -1) score = "Advantage player2";
             else if (minusResult >= 2) score = "Win for player1";
             else score = "Win for player2";
         } else {
+            if (m_score1 == m_score2)
+                return SCORE_NAMES[m_score1] + "-All";
             return SCORE_NAMES[m_score1] + "-" + SCORE_NAMES[m_score2];
 
         }
