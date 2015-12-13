43617ba9ba515a01555ea0e5c200275b54791171
Philipp Eder <ederphpp@gmail.com>
2015-12-13 22:46:35 +0100

    created team and game classes to make it easier to stream if somebody wants to get rid of those unnecessary "getScore" calls in the tests

--- src/main/java/de/egga/TennisGame1.java
+++ src/main/java/de/egga/TennisGame1.java
@@ -2,39 +2,82 @@ package de.egga;
 
 public class TennisGame1 implements TennisGame {
 
-    private int scoreOfTeamOne = 0;
-    private int scoreOfTeamTwo = 0;
-    private final String teamOne;
-    private final String teamTwo;
+    private static class Team{
+        private final String name;
+        private final int score;
 
-    private final String[] SCORE_NAMES = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        private Team(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        static Team addScore(Team team){
+            return new Team(team.name, team.score + 1);
+        }
+
+
+    }
+
+    private class Game{
+        private final Team one;
+        private final Team two;
+
+        public Game(String nameOfTeamOne, String nameOfTeamTwo) {
+            one = new Team(nameOfTeamOne, 0);
+            two = new Team(nameOfTeamTwo, 0);
+        }
+
+        private Game(Team one, Team two) {
+            this.one = one;
+            this.two = two;
+        }
+        
+
+        private final String[] SCORE_NAMES = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+
+
+        public Game wonPoint(String playerName) {
+
+            if (one.name.equals(playerName))
+                return new Game(Team.addScore(one), two);
+            else
+                return new Game(one, Team.addScore(two));
+        }
+
+        public String getScore() {
+
+            if (one.score == 3 && two.score == 3 || one.score == 4 && two.score == 4)
+                return "Deuce";
+            if (one.score >= 4 || two.score >= 4) {
+                if (Math.abs(one.score - two.score)  == 1)
+                    return "Advantage " + (one.score > two.score ? one.name : two.name);
+                return "Win for " + (one.score > two.score ? one.name : two.name);
+            }
+            if (one.score == two.score)
+                return SCORE_NAMES[one.score] + "-All";
+            return SCORE_NAMES[one.score] + "-" + SCORE_NAMES[two.score];
+
+
+        }
+
+
+    }
+
+    private Game game;
 
 
     public TennisGame1(String teamOne, String teamTwo) {
-        this.teamOne = teamOne;
-        this.teamTwo = teamTwo;
+        game = new Game(teamOne, teamTwo);
+
     }
 
     public void wonPoint(String playerName) {
-        if (teamOne.equals(playerName))
-            scoreOfTeamOne++;
-        else
-            scoreOfTeamTwo++;
+        game = game.wonPoint(playerName);
+
     }
 
     public String getScore() {
-
-        if (scoreOfTeamOne == 3 && scoreOfTeamTwo == 3 || scoreOfTeamOne == 4 && scoreOfTeamTwo == 4)
-            return "Deuce";
-        if (scoreOfTeamOne >= 4 || scoreOfTeamTwo >= 4) {
-            if (Math.abs(scoreOfTeamOne - scoreOfTeamTwo)  == 1)
-                return "Advantage " + (scoreOfTeamOne > scoreOfTeamTwo ? teamOne : teamTwo);
-            return "Win for " + (scoreOfTeamOne > scoreOfTeamTwo ? teamOne : teamTwo);
-        }
-        if (scoreOfTeamOne == scoreOfTeamTwo)
-            return SCORE_NAMES[scoreOfTeamOne] + "-All";
-        return SCORE_NAMES[scoreOfTeamOne] + "-" + SCORE_NAMES[scoreOfTeamTwo];
-
+        return game.getScore();
 
     }
 }
