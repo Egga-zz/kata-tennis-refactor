fe75b6b56c60a706221485089281b1632038b333
Egga (Jan Hartung) <github@egga.de>
2015-12-14 13:37:24 +0100

    use equals() for String comparison

--- src/main/java/de/egga/TennisGame1.java
+++ src/main/java/de/egga/TennisGame1.java
@@ -1,12 +1,14 @@
 package de.egga;
 
+import java.util.Objects;
+
 public class TennisGame1 implements TennisGame {
 
     private int m_score1 = 0;
     private int m_score2 = 0;
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (Objects.equals(playerName, "player1"))
             m_score1 += 1;
         else
             m_score2 += 1;
