883107ddf85c22cdb6e5722617169366d7f5c16e
Egga (Jan Hartung) <github@egga.de>
2015-12-14 13:41:10 +0100

    other equals() please

--- src/main/java/de/egga/TennisGame1.java
+++ src/main/java/de/egga/TennisGame1.java
@@ -1,14 +1,12 @@
 package de.egga;
 
-import java.util.Objects;
-
 public class TennisGame1 implements TennisGame {
 
     private int m_score1 = 0;
     private int m_score2 = 0;
 
     public void wonPoint(String playerName) {
-        if (Objects.equals(playerName, "player1"))
+        if (playerName.equals("player1"))
             m_score1 += 1;
         else
             m_score2 += 1;
