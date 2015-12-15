fd18a9c95186834aff6c63400dffabd56098a8d8
Ralph O. Delfs <r.o.delfs@web.de>
2015-12-15 14:35:06 +0100

    changes

--- src/main/java/de/egga/TennisGame1.java
+++ src/main/java/de/egga/TennisGame1.java
@@ -2,10 +2,13 @@ package de.egga;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private static final int MAX_SCORE = 4;
+
+    private int player1RawScore = 0;
+    private int player2RawScore = 0;
     private String player1Name;
     private String player2Name;
+
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,65 +16,71 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        // NOTE: check against actual player names
+        if (playerName == this.player1Name) {
+            player1RawScore += 1;
+        } else if (playerName == this.player2Name) {
+            player2RawScore += 1;
+        }
+        // TODO: throw exception for unknown name
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
 
-            }
+        // NOTE: logig moved to private functions
+        if (player1RawScore==player2RawScore) {
+            score = equalScoreToString(player1RawScore);
+        } else if (player1RawScore>=MAX_SCORE || player2RawScore>=MAX_SCORE) {
+            score = tiedScoreToString(player1RawScore, player2RawScore);
+        } else {
+            score = playerScoreToString(player1RawScore) + "-" + playerScoreToString(player2RawScore);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+
         return score;
     }
+
+    private String equalScoreToString(int mScore) {
+        switch (mScore)
+        {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+        }
+        return "Deuce";
+    }
+
+    private String tiedScoreToString(int p1Score, int p2Score) {
+        int scoreDifference = p1Score - p2Score;
+        if (scoreDifference == 1) {
+            return "Advantage " + this.player1Name;
+        } else if (scoreDifference == -1) {
+            return "Advantage " + this.player2Name;
+        } else if (scoreDifference >= 2) {
+            return "Win for " + this.player1Name;
+        }
+        return "Win for " + this.player2Name;
+    }
+
+    private String playerScoreToString(int mScore) {
+        // NOTE: unintuitive loop construct removed
+        // NOTE: breaks removed
+        switch(mScore)
+        {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
+        // TODO: Throw exception
+        return "Undefined";
+    }
+
 }
